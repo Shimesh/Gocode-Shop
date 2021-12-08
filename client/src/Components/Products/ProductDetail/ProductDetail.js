1c634ef2c88182f41318075dd01069d29a21ed84
Shimesh <yaron.orb@gmail.com>
2021-12-08 19:24:39 +0200

    productDetail

--- client/src/Components/Products/ProductDetail/ProductDetail.js
+++ client/src/Components/Products/ProductDetail/ProductDetail.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router";
+import { useState } from "react/cjs/react.development";
 
 const ProductDetail = () => {
+  const [product, setProduct] = useState({});
   const { id } = useParams();
-  // const product = async () => {
-  //   const res = await fetch(`/api/products/${id}`);
-  //   const p = await res.json();
+  const fetchProducts = async () => {
+    const res = await fetch(`/api/products/${id}`);
+    const product = await res.json();
+    setProduct(product);
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
-  //   const { title, price, image } = p;
-
-  // };
   return (
     <>
+      <h1>{product.title}</h1>
+      <h2>{product.price}</h2>
+      <img src={product.image} alt={product.title} style={{ width: "300px" }} />
       <h5>id num :{id}</h5>
     </>
   );
